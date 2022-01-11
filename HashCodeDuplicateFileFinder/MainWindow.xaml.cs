05a9a1972cf10a6ff12be5bb6421a202f4927b50
Barby <barbara.erdec@gmail.com>
2022-01-11 18:49:41 +0100

    Installer setup

--- HashCodeDuplicateFileFinder/MainWindow.xaml.cs
+++ HashCodeDuplicateFileFinder/MainWindow.xaml.cs
@@ -1,40 +1,17 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 
 using System.Linq;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
-using System.Xml.XPath;
 
 
 using Microsoft.WindowsAPICodePack.Dialogs;
 
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static Microsoft.WindowsAPICodePack.Shell.PropertySystem.SystemProperties.System;
-
-using MenuItem = System.Windows.Forms.MenuItem;
-
 namespace HashCodeDuplicateFileFinder
-
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
